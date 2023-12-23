54ed2321470f9d21c5a59866ca2aef7c797cbcb4
Av3z <willyanf17@hotmail.com>
2023-12-22 22:17:56 -0300

    test: new test in repository

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,25 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:dio_imc/src/app.dart';
-import 'package:flutter/material.dart';
+import 'package:dio_imc/src/repositories/imc_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  ImcRepository imcRepository = ImcRepository();
+  imcRepository.setImcList(2.2);
+  imcRepository.setImcList(4.2);
+  imcRepository.setImcList(2.7);
+  imcRepository.setImcList(10.28);
+  test('IMC repository ', () => {expect(imcRepository.getListImc().length, 4)});
 }
